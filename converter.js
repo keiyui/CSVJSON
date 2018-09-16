7877ba94cf699acecaabeaf8614a2476c6e762a0
keiyui <43130336+keiyui@users.noreply.github.com>
2018-09-16 19:55:49 +0800

    Delete converter.js

--- converter.js
+++
@@ -1,9 +0,0 @@
-const csv = require('csvtojson')
-const fs = require('fs')
-
-csv().fromFile('./customer-data.csv').then((jsonArray) => {
-    fs.writeFile('customer-data.json', JSON.stringify(jsonArray), (error) => {
-        if(error) return console.error(error)
-        console.log('Writing done')
-    })
-})
